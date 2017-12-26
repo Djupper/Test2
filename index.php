a5be111f972a7143e76511f830de0aa5dc150ce4
Djupper <Djupper@mail.ru>
2017-12-26 12:37:18 +0700

    26.17

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-<p>
-    <?php
-    echo "Привет Git!";
-    $today = date("Ymd");
-    echo $today;
-    ?>
-</p>
+<?php
+require_once 'mpdf60/mpdf.php';
+$text =  require_once '1.html';
+$text='<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /></head><body>'.$html.'</body></html>';
 
-</body>
-</html>
+$mpdf = new \mPDF('utf-8', 'A4', '0', 'serif','5', '-10', '5', '5', '0', '0', 'L');
+
+$mpdf->CSSselectMedia='pdf';
+
+$mpdf->WriteHTML($text, 0);
+
+$file_name = $_SERVER['DOCUMENT_ROOT'].'/pdf/test.pdf';
+
+$mpdf->Output($file_name);
+?>
