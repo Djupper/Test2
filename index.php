48a28936eed2c2a5b7bf347824b2884b041abc28
Djupper <Djupper@mail.ru>
2017-12-28 14:23:33 +0700

    27.17

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <?php
 require_once 'mpdf60/mpdf.php';
-$text =  require_once '1.html';
-$text='<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /></head><body>'.$html.'</body></html>';
+//$html =  require_once '2.html';
+ob_start();
+include_once('5.html');
+$html = ob_get_contents();
+ob_end_clean();
 
-$mpdf = new \mPDF('utf-8', 'A4', '0', 'serif','5', '-10', '5', '5', '0', '0', 'L');
 
-$mpdf->CSSselectMedia='pdf';
+$text = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" 
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"></head><body>' . $html . '</body></html>';
+
+$mpdf = new \mPDF('utf-8', 'A4', '0', 'serif', '5', '-10', '5', '5', '0', '0', 'L');
+
+$mpdf->CSSselectMedia = 'pdf';
 
 $mpdf->WriteHTML($text, 0);
 
-$file_name = $_SERVER['DOCUMENT_ROOT'].'/pdf/test.pdf';
+$file_name = $_SERVER['DOCUMENT_ROOT'] . '/pdf/test.pdf';
 
 $mpdf->Output($file_name);
 ?>
